41bf3f9eaf00bf8ecee5dd2831c335fd0bff98e5
Yosef Deray <yosefderay770@gmail.com>
2014-10-20 00:19:50 -0400

    got the ships working

--- src/board.rs
+++ src/board.rs
@@ -38,11 +38,73 @@ impl<T: Ship> Board<T> {
 
 pub trait Ship {
 
+    fn contains(&self, point: Point) -> bool;
+    fn hit(&mut self) -> bool;
 }
 
 #[deriving(Show)]
-pub struct DefaultShip;
+pub struct DefaultShip {
+
+    pub point1: Point,
+    pub point2: Point,
+    hits: int,
+    max_hits: int
+}
+
+impl DefaultShip {
+
+    pub fn new(point1: Point, point2: Point) -> DefaultShip {
+
+        DefaultShip {
+
+            point1: point1,
+            point2: point2,
+            hits: 0,
+            max_hits: DefaultShip::get_distance(point1, point2) + 1
+        }
+    }
+
+    fn get_distance(point1: Point, point2: Point) -> int {
+
+        ((point1.x - point2.x) as f32).hypot((point1.y - point2.y) as f32) as int
+    }
+}
 
 impl Ship for DefaultShip {
 
+    fn contains(&self, point: Point) -> bool {
+
+        DefaultShip::get_distance(self.point1, point) + DefaultShip::get_distance(point, self.point2) + 1 == self.max_hits
+    }
+
+    fn hit(&mut self) -> bool {
+
+        self.hits += 1;
+
+        if (self.hits >= self.max_hits) {
+
+            return true
+        }
+
+        false
+    }
+}
+
+#[deriving(Show)]
+pub struct Point {
+
+    pub x: int,
+    pub y: int
+}
+
+impl Point {
+
+    pub fn new(x: int, y: int) -> Point {
+
+        Point {
+
+            x: x,
+            y: y
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,9 @@ use std::io;
 
 fn main() {
 
-    println!("{}", board::Board::new(10, 10, Vec<board::Ship>::new()))
+    println!("{}", board::Board::new(10, 10, vec![board::DefaultShip::new(
+
+        board::Point::new(10, 20),
+        board::Point::new(10, 21)
+    )]))
 }
