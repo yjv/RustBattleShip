1c6566b0f8c5fb4cf9abc033c7072e5734b2436b
Yosef Deray <yosefderay770@gmail.com>
2014-11-02 01:38:30 -0400

    got the basic gameplay working

--- src/executor.rs
+++ src/executor.rs
@@ -1,8 +1,36 @@
 use board;
+use std::io;
 
 pub trait Input {
 
     fn get_point(&self) -> board::Point;
+}
+
+pub struct CliInput;
+
+impl CliInput {
+
+    pub fn new() -> CliInput {
+
+        CliInput
+    }
+}
+
+impl Input for CliInput {
+
+    fn get_point(&self) -> board::Point {
+
+        println!("Guess row: ");
+        let y = from_str::<int>(io::stdin().read_line().ok().expect("Failed to read line").as_slice().trim()).unwrap() - 1;
+        println!("Guess column: ");
+        let x = from_str::<int>(io::stdin().read_line().ok().expect("Failed to read line").as_slice().trim()).unwrap() - 1;
+
+        board::Point {
+
+            x: x,
+            y: y
+        }
+    }
 }
 
 pub trait Output<T: board::Ship> {
@@ -15,7 +43,69 @@ pub trait Output<T: board::Ship> {
     fn lost(&self);
     fn won(&self);
     fn error(&self, message: String);
-    fn turn(&self, turn: int, max_turn: int);
+    fn turn(&self, turn: uint, max_turn: uint);
+}
+
+pub struct CliOutput;
+
+impl CliOutput {
+
+    pub fn new() -> CliOutput {
+
+        CliOutput
+    }
+}
+
+impl <T: board::Ship> Output<T> for CliOutput {
+
+    fn welcome(&self, board: &board::Board<T>) {
+
+        println!(
+            "Welcome to mini battleship! You have ten turns to egusss where my battleship is on this {} by {} square by by guessing a row and a column",
+            board.grid.width,
+            board.grid.height
+        );
+    }
+
+    fn grid(&self, grid: &board::Grid) {
+
+        println!("{}", grid);
+    }
+
+    fn hit(&self) {
+
+        println!("Hit!");
+    }
+
+    fn sink(&self) {
+
+        println!("Sunk!");
+    }
+
+    fn miss(&self) {
+
+        println!("You missed my battleship");
+    }
+
+    fn lost(&self) {
+
+        println!("Game over. So sorry. You lost.");
+    }
+
+    fn won(&self) {
+
+        println!("Congratulations! You sunk all my battleships!");
+    }
+
+    fn error(&self, message: String) {
+
+        println!("Oops, {}", message);
+    }
+
+    fn turn(&self, turn: uint, max_turns: uint) {
+
+        println!("Turn {} of {}", turn, max_turns);
+    }
 }
 
 pub struct Executor<T: Input, U: Output<V>, V: board::Ship> {
@@ -37,7 +127,7 @@ impl <T: Input, U: Output<V>, V: board::Ship> Executor<T, U, V> {
         }
     }
 
-    pub fn execute(&mut self, max_turns: int) {
+    pub fn execute(&mut self, max_turns: uint) {
 
         self.output.welcome(&self.board);
 
--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,15 @@ extern crate battle_ship;
 extern crate collections;
 
 use battle_ship::board;
+use battle_ship::executor;
 
 fn main() {
 
-    let mut board = board::Board::new(10, 10, vec![board::DefaultShip::new(
+    let mut executor = executor::Executor::new(board::Board::new(10, 10, vec![board::DefaultShip::new(
 
         board::Point::new(1, 2),
         board::Point::new(1, 3)
-    )]);
-    println!("{}", board);
+    )]), executor::CliInput::new(), executor::CliOutput::new());
 
-    println!("{}", board.fire(board::Point::new(1,2)));
-
-    println!("{}", board);
-
-    println!("all sunk? {}", board.all_sunk());
-    println!("{}", board.fire(board::Point::new(2,2)));
-
-    println!("{}", board);
-
-    println!("all sunk? {}", board.all_sunk());
-    println!("{}", board.fire(board::Point::new(1,3)));
-
-    println!("{}", board);
-
-    println!("all sunk? {}", board.all_sunk());
+    executor.execute(10);
 }
